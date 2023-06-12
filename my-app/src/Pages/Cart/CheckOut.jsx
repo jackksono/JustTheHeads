2f9bb3cf4b07f95ad71caf7b889cf0ee19bae826
Jack Sonoda <jackksono@gmail.com>
2023-06-11 21:28:37 -0700

    Fixed checkout w/ updated cart items responding to no items

--- my-app/src/Pages/Cart/CheckOut.jsx
+++ my-app/src/Pages/Cart/CheckOut.jsx
@@ -12,25 +12,31 @@ const CheckOut = () => {
 
     const { cartItems, getTotal } = useContext(ShopContext)
     return (
+       
+           
+        
         <div className="cart flex flex-col justify-center items-center  pt-48 text-lg">
+            {getTotal() > 0 ?
             <div>
                 <h1 className="text-center text-black text-3xl">Your Cart Items</h1>
             </div>
-            <div className="cartItems">
+            : <></>}
+            <div>
+
                 {PRODUCTS.map((product) => {
                     if (cartItems[product.id] !== 0) {
                         return <CartItem data={product}></CartItem>
                     }})}
             </div>
-    {getTotal() > 0 ?
+            {getTotal() > 0 ?
+
             <div className="">
                 <p className="text-black"> Subtotal: ${getTotal()}</p>
                 <button onClick={() => navigate('/all-products')} className="w-[200px] h-[50px] bg-black text-white rounded-[8px] m-[10px] cursor-pointer"> Continue Shopping </button>
                 <button className="w-[200px] h-[50px] bg-black text-white rounded-[8px] m-[10px] cursor-pointer"> Checkout </button>
             </div>
-            : <h1>Your Cart is Empty</h1>}
+            : <h1 className="text-black">Your Cart is Empty</h1>}
         </div>
-        
     )
 }
 
