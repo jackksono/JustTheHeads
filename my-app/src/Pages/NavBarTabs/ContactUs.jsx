942a40bb15b0bb6c100d93151b79719fa22acd11
Jack Sonoda <jackksono@gmail.com>
2023-07-01 14:07:40 -0700

    Updated contact us to include real email addresses

--- my-app/src/Pages/NavBarTabs/ContactUs.jsx
+++ my-app/src/Pages/NavBarTabs/ContactUs.jsx
@@ -6,6 +6,7 @@ const ContactUs = (props) => {
     const [ data, setData ] = useState({name:"", email:"", phone:""})
     const [ message, setMessage ] = useState()
     const [ result, showResult ] = useState(false)
+    const [ validEmail, setValidEmail ] = useState(false)
 
     // const ref = useClickAway(() => {
     //     const close = () => props.setTrigger(false);
@@ -21,20 +22,39 @@ const ContactUs = (props) => {
         )
     }
 
+    const EmailResponse = () => {
+        return (
+            <p>Please enter a valid email</p>
+        )
+    }
+
     const messageChange = (e) => {
         setMessage(e.target.value)
+    }
+
+    const twoCalls = (e) => {
+        onChangeValidEmail();
+        handleChange(e)
+    }
+
+    const onChangeValidEmail = () => {
+        let re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (re.test(data.email)) {setValidEmail(false)}
+        else setValidEmail(true)
     }
 
     const handleChange = (e) => {
         const name = e.target.name
         const value = e.target.value
         setData({...data, [name]:value})
+        
+        
     }
 
     const sendMail = (e) => { //EmailJS documentation
         e.preventDefault()
-
-        emailjs
+        if(!validEmail) {
+            emailjs
         .sendForm('service_0bd37pt', 'template_sasqli3', e.target, 'iauEh19za_KMIe1hP')
         .then((result) => {
             console.log(result.text);
@@ -45,45 +65,50 @@ const ContactUs = (props) => {
         setData("")
         setMessage("")
         showResult(true)
+    }
+    else (setValidEmail(true))
+        
     };
 
     return (props.trigger) ? (
-        <div className="fixed overflow-auto inset-x-0 mx-auto w-1/2 h-[700px] bg-red-900 bg-opacity-90 rounded-xl shadow-2xl duration-500" onClick={click} >
+        <div className="fixed overflow-auto inset-x-0 mx-auto w-1/2 h-[720px] bg-orange-600 bg-opacity-90 rounded-xl shadow-2xl duration-500" onClick={click} >
        
         <form className="" method="post" onSubmit={sendMail}>
-            <div className='flex pr-5 justify-end text-2xl text-black'>    
+            <div className='flex justify-end pr-5 text-2xl text-black'>    
                 <button  onClick={() => props.setTrigger(false)}>X</button>
             </div>
-            <h1 className="flex justify-center text-white pl-10 text-4xl">How's The <span className="text-orange-600 italic underline">Head?</span></h1>
+            <h1 className="flex justify-center pl-10 text-4xl text-white">How's The <span className="italic text-red-800 underline">Head?</span></h1>
             
                 
-            <div className="text-lg text-white pl-6 italic">
+            <div className="pl-6 text-lg italic text-black">
                 <span className="pl-3">Name:</span>
-                <input className="m-3 justify-center w-11/12 p-1 flex border-solid border-2 border-black text-lg rounded-xl shadow-lg" 
+                <input className="flex justify-center w-11/12 p-1 m-3 text-lg border-2 border-black border-solid shadow-lg focus:outline-none focus:border-rose-600 focus:border-4 bg-opacity-90 rounded-xl" 
                 type='text' name='name' id="" onChange={handleChange} value={data.name} placeholder="Enter name"/>
             </div>
 
-            <div className="text-lg text-white pl-6 italic">
+            <div className="pl-6 text-lg italic text-black">
                 <span className="pl-3">Email:</span>
-                <input className="m-3 justify-center w-11/12 p-1 flex border-solid border-2 border-black text-lg rounded-xl shadow-lg" 
-                type='email' name='email' id="" onChange={handleChange} value={data.email} placeholder="example@gmail.com"/>
+                <input className="flex justify-center w-11/12 p-1 m-3 text-lg border-2 border-black border-solid shadow-lg focus:outline-none focus:border-rose-600 focus:border-4 rounded-xl" 
+                type='email' name='email' id="" onChange={twoCalls}  value={data.email} placeholder="example@gmail.com"/>
+                <h2 className= "text-xl font-semibold text-center text-red-900 ">{validEmail ? <EmailResponse/> : null }</h2>  {/*Hidden message has been sent*/}
             </div>
 
-            <div className="text-lg text-white pl-6 italic">
+            <div className="pl-6 text-lg italic text-black">
                 <span className="pl-3">Phone#:</span>
-                <input className="m-3 justify-center w-11/12 p-1 flex border-solid border-2 border-black text-lg rounded-xl shadow-lg" 
+                <input className="flex justify-center w-11/12 p-1 m-3 text-lg border-2 border-black border-solid shadow-lg focus:outline-none focus:border-rose-600 focus:border-4 rounded-xl" 
                 type='phone' name='phone' id="" onChange={handleChange} value={data.phone} placeholder="+1 (xxx)-xxx-xxxx"/>
             </div>
 
-            <div className="text-lg text-white pl-6 italic">
+            <div className="pl-6 text-lg italic text-black">
                 <span className="pl-3">Message:</span>
-                <textarea className="border-solid border-2 p-1 border-black m-3 justify-center w-11/12  flex text-lg rounded-xl shadow-lg" 
+                <textarea className="flex justify-center w-11/12 p-1 m-3 text-lg border-2 border-black border-solid shadow-lg focus:outline-none focus:border-rose-600 focus:border-4 rounded-xl" 
                 name='message' id='' cols='30' rows='7' onChange={messageChange} value={message} placeholder="Enter message here..."/>
             </div>
             
-            <h2 className= "pt-5 text-lg text-white text-center justify-center">{result ? <Result/> : null}</h2>  {/*Hidden message has been sent*/}
-            <div className="justify-center items-center text-center">
-                <button className="w-1/4 p-2 bg-cyan-500 text-white rounded-xl hover:text-cyan-500 hover:bg-white" type='submit'>Send</button>
+            <h2 className= "justify-center pb-2 text-lg text-center text-white">{result ? <Result/> : null}</h2>
+            
+            <div className="items-center justify-center text-center">
+                <button className="w-1/4 p-2 text-white bg-cyan-500 rounded-xl hover:text-cyan-500 hover:bg-white" type='submit'>Send</button>
             </div>    
          </form>  
     </div>
--- my-app/src/Products/AllProducts.jsx
+++ my-app/src/Products/AllProducts.jsx
@@ -8,15 +8,15 @@ import Footer from "../Pages/Footer";
 const AllProducts = () => {
     return (
         <div className="pt-44">
-            <div className="relative text-black text-center text-7xl font-YsabeauSC">
+            <div className="relative text-center text-black text-7xl font-YsabeauSC">
                 <h1 className="text center">All Products</h1>
             </div>
-            <div className="grid grid-cols-3 w-full h-auto place-items-center pt-12">
+            <div className="grid w-full h-auto grid-cols-3 pt-12 place-items-center">
                 {PRODUCTS.map((product) => (
                 <Product data={product}/>
             ))}
             </div>
-            <footer className=" bottom-0">
+            <footer className="bottom-0 ">
                 <Footer/>
             </footer>
         </div>
