569180790d0c15e561138ecf6bd67fb6ef756773
Jack Sonoda <jackksono@gmail.com>
2023-10-06 17:17:03 -0700

    Loading update

--- src/Pages/Fallback/Loading.jsx
+++ src/Pages/Fallback/Loading.jsx
@@ -5,6 +5,7 @@ const Loading = () => {
   return (
     <div className='fixed flex flex-col items-center justify-center w-screen h-screen bg-color-background'>
         <p className='text-color-text'>Loading</p>
+        <p className='text-color-text'>Please wait</p>
         <AiOutlineLoading3Quarters className='flex animate-spin'></AiOutlineLoading3Quarters>
     </div>
   )
